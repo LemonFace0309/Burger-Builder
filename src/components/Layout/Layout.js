e4a173129c4a17b5c3533ec8ddfcccb31530fea4
Charles Liu <charles.liu0309@gmail.com>
2020-12-22 16:42:08 -0500

    added css transitions to sidedrawer and replaced Menu placeholder with a taggleable hamburger icon for smart phones

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Layout.module.css'
 import Aux from '../../hoc/Auxiliary'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true,
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
 
-export default layout
+    sideDrawerToggleHandler = () => {
+        this.setState( prevState => ({showSideDrawer: !prevState.showSideDrawer})
+        // function is asyncronous so needs prevState argument to avoid bugs
+        );
+    }
+    
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggle={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+};
+
+export default Layout
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
+            <DrawerToggle clicked={props.toggle} />
             <div className={classes.Logo}>
                 <Logo />
             </div>
