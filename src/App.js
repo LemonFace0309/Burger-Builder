3f2560da73433c17053cc213bbac19f513b376a4
LemonFace0309 <charles.liu0309@gmail.com>
2020-12-26 01:14:22 -0500

    hopefully basename works as intended now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Layout from './hoc/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 
-function App() {
+const App = (props) => {
   return (
     <div>
       <Layout>
         <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
+          <Route path='/' exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
         </Switch>
       </Layout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
