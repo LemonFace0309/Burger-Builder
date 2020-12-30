8bb3f360dacf77f721e4d14b50f1bf3017512f44
LemonFace0309 <charles.liu0309@gmail.com>
2020-12-30 02:13:13 -0500

    added route protection with guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import * as actionCreators from './store/actions/index'
@@ -14,21 +14,37 @@ class App extends Component {
   componentDidMount() {
     this.props.onTryAutoLogin()
   }
-  
+
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    )
+    if (this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
     return (
       <div>
-        <Layout>
-          <Switch>
-            <Route path="/" exact component={BurgerBuilder} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-          </Switch>
-        </Layout>
+        <Layout>{routes}</Layout>
       </div>
     )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.token !== null,
   }
 }
 
@@ -38,4 +54,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
