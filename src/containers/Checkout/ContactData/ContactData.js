2ccd096b20edbacc70532c02ecc0e7631bc18547
LemonFace0309 <charles.liu0309@gmail.com>
2021-01-01 19:49:44 -0500

    refractored ContactData.js to a functional component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import axios from '../../../axios'
@@ -10,91 +10,87 @@ import Input from '../../../components/UI/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actionCreators from '../../../store/actions/index'
 
-class Name extends Component {
-  constructor() {
-    super()
-    const formField = {
-      elType: 'input',
-      elConfig: {
-        type: 'text',
-        placeholder: '',
-      },
-      value: '',
-      validation: {
-        required: true,
-      },
-      valid: false,
-      touched: false,
-    }
-    const usersName = { ...formField }
-    usersName.elConfig = {
+const ContactData = (props) => {
+  const formField = {
+    elType: 'input',
+    elConfig: {
       type: 'text',
-      placeholder: 'Your Name',
-    }
-    const street = { ...formField }
-    street.elConfig = {
-      type: 'text',
-      placeholder: 'Street',
-    }
-    const postalCode = { ...formField }
-    postalCode.elConfig = {
-      type: 'text',
-      placeholder: 'Postal Code',
-    }
-    const country = { ...formField }
-    country.elConfig = {
-      type: 'text',
-      placeholder: 'Country',
-    }
-    const email = { ...formField }
-    email.elConfig = {
-      type: 'text',
-      placeholder: 'Email',
-    }
-    const deliveryMethod = { ...formField }
-    deliveryMethod.elType = 'select'
-    deliveryMethod.elConfig = {
-      options: [
-        { value: 'fastest', displayValue: 'Fastest' },
-        { value: 'poor', displayValue: 'Poor' },
-      ],
-    }
-    deliveryMethod.value = "fastest"
-    deliveryMethod.valid = true
-
-    this.state = {
-      orderForm: {
-        name: usersName,
-        street: street,
-        postalCode: postalCode,
-        country: country,
-        email: email,
-        deliveryMethod: deliveryMethod,
-      },
-      formIsValid: false,
-    }
+      placeholder: '',
+    },
+    value: '',
+    validation: {
+      required: true,
+    },
+    valid: false,
+    touched: false,
   }
+  const usersName = { ...formField }
+  usersName.elConfig = {
+    type: 'text',
+    placeholder: 'Your Name',
+  }
+  const street = { ...formField }
+  street.elConfig = {
+    type: 'text',
+    placeholder: 'Street',
+  }
+  const postalCode = { ...formField }
+  postalCode.elConfig = {
+    type: 'text',
+    placeholder: 'Postal Code',
+  }
+  const country = { ...formField }
+  country.elConfig = {
+    type: 'text',
+    placeholder: 'Country',
+  }
+  const email = { ...formField }
+  email.elConfig = {
+    type: 'text',
+    placeholder: 'Email',
+  }
+  const deliveryMethod = { ...formField }
+  deliveryMethod.elType = 'select'
+  deliveryMethod.elConfig = {
+    options: [
+      { value: 'fastest', displayValue: 'Fastest' },
+      { value: 'poor', displayValue: 'Poor' },
+    ],
+  }
+  deliveryMethod.value = 'fastest'
+  deliveryMethod.valid = true
 
-  orderHandler = (e) => {
+  const [orderForm, setOrderForm] = useState({
+    name: usersName,
+    street: street,
+    postalCode: postalCode,
+    country: country,
+    email: email,
+    deliveryMethod: deliveryMethod,
+  })
+
+  const [formIsValid, setFormIsValid] = useState(false)
+
+  const orderHandler = (e) => {
     e.preventDefault()
     const formData = {}
-    for (let key in this.state.orderForm) {
-      formData[key] = this.state.orderForm[key].value
+    for (let key in orderForm) {
+      formData[key] = orderForm[key].value
     }
     const order = {
-      userId: this.props.userId,
-      ingredients: this.props.ingredients,
-      price: this.props.totalPrice,
+      userId: props.userId,
+      ingredients: props.ingredients,
+      price: props.totalPrice,
       // do not sent price data like this in production. Calculate total price on server;
       // user may try to modify totalPrice before request is sent.
       orderData: formData,
     }
-    this.props.onPurchaseBurger(order, this.props.token)
+    props.onPurchaseBurger(order, props.token)
   }
 
-  inputChangedHandler = (e, key) => {
+  const inputChangedHandler = (e, key) => {
     const updatedOrderForm = {
-      ...this.state.orderForm,
+      ...orderForm,
     }
     const updatedFormEl = { ...updatedOrderForm[key] }
     updatedFormEl.value = e.target.value
@@ -109,41 +105,42 @@ class Name extends Component {
       return acc && updatedOrderForm[key].valid
     }, true)
 
-    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid })
+    setOrderForm(updatedOrderForm)
+    setFormIsValid(formIsValid)
   }
 
-  render() {
-    const formElsArray = Object.keys(this.state.orderForm).map((key) => {
-      const formEl = this.state.orderForm[key]
-      return (
-        <Input
-          key={key}
-          elType={formEl.elType}
-          elConfig={formEl.elConfig}
-          value={formEl.value}
-          invalid={!formEl.valid}
-          touched={formEl.touched}
-          changed={(e) => this.inputChangedHandler(e, key)}
-        />
-      )
-    })
-
-    let form = (
-      <form onSubmit={this.orderHandler}>
-        {formElsArray}
-        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
-      </form>
-    )
-    if (this.props.loading) {
-      form = <Spinner />
-    }
+  const formElsArray = Object.keys(orderForm).map((key) => {
+    const formEl = orderForm[key]
     return (
-      <div className={classes.ContactData}>
-        <h4>Enter your Contact Information</h4>
-        {form}
-      </div>
+      <Input
+        key={key}
+        elType={formEl.elType}
+        elConfig={formEl.elConfig}
+        value={formEl.value}
+        invalid={!formEl.valid}
+        touched={formEl.touched}
+        changed={(e) => inputChangedHandler(e, key)}
+      />
     )
+  })
+
+  let form = (
+    <form onSubmit={orderHandler}>
+      {formElsArray}
+      <Button btnType="Success" disabled={!formIsValid}>
+        ORDER
+      </Button>
+    </form>
+  )
+  if (props.loading) {
+    form = <Spinner />
   }
+  return (
+    <div className={classes.ContactData}>
+      <h4>Enter your Contact Information</h4>
+      {form}
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
@@ -158,8 +155,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onPurchaseBurger: (orderData, token) => dispatch(actionCreators.purchaseBurger(orderData, token))
+    onPurchaseBurger: (orderData, token) =>
+      dispatch(actionCreators.purchaseBurger(orderData, token)),
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Name, axios))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withErrorHandler(ContactData, axios))
